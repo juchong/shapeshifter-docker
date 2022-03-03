9cb9fab364b26eaf1e086c18077a73308eba04a7
Juan Chong <juan@plainmail.net>
2022-03-02 22:01:01 -0800

    Trimming options

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
 exec /go/bin/shapeshifter-dispatcher \
-    -$PMODE \
-    -$OMODE \
-    -state /config/ \
-    -orport $ORPORT \
-    -transports $TRANSPORTS \
-    -bindaddr $TRANSPORTS-$BINDADDR \
     $ARGS
